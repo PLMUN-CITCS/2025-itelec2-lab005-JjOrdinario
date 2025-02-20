6a6126f771de8745a1467ab754314cf65aa2f32a
JjOrdinario <johnjenricordi@gmail.com>
2025-02-20 21:39:38 +0800

    Submitting Python Week 02 - Session 01 - Exercise 03

--- exercise_03.py
+++ exercise_03.py
@@ -8,6 +8,9 @@ print("Result of a + b * c:", result1)
 result2 = (a + b) * c
 print("Result of (a + b) * c:", result2)
 
+result3 = a - b
+print("Result of a - b:", result3)
+
 result4 = a / b
 result5 = a // b
 print("Result of a / b:", result4)
